645e21348cfb864bc371e3fbdc97836b13a148a4
Emile Halliez <ehalliez@2A6.42angouleme.fr>
2024-03-11 19:11:34 +0100

    add

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,16 @@
 /*   By: ehalliez <ehalliez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 15:54:01 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/03/11 19:10:02 by ehalliez         ###   ########.fr       */
+/*   Updated: 2024/03/11 19:11:25 by ehalliez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <libwati.h>
 
-void print(char *str)
+void print(void *str)
 {
-	wati_printf("%s", str);
+	wati_putendl_fd(str, 0);
 }
 
 int	main(int argc, char **argv)
