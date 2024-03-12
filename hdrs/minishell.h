80e6e48560ae15574220bb9b60e3c8ba9b825e72
Emile Halliez <ehalliez@2A6.42angouleme.fr>
2024-03-12 20:45:11 +0100

    added parsing

--- hdrs/minishell.h
+++ hdrs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ehalliez <ehalliez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 16:30:42 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/03/12 20:39:59 by ehalliez         ###   ########.fr       */
+/*   Updated: 2024/03/12 20:43:55 by ehalliez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,17 @@ typedef struct s_test
 }	t_test;
 
 // #### PARSING ### // 
+
+char	*ft_join_args(char **argv);
+char	*get_operator(char **line);
+char	*skip_space(char **line);
+char	*get_word(char **line, t_test *test);
+char	*get_next_token(char **line, t_test *test);
+
+void	print(void *str);
+
+int		is_char_operator(char c);
+
+t_list	*init_parsing(char *line);
+
 #endif
