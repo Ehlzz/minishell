074a932a2e6285897b4a1d77edd9b2d39f81ba94
Benoit Darenne <bedarenn@3F3.42angouleme.fr>
2024-03-12 17:25:57 +0100

    add valgrind supp finish main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bedarenn <bedarenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 15:54:01 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/03/12 17:07:52 by bedarenn         ###   ########.fr       */
+/*   Updated: 2024/03/12 17:25:28 by bedarenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,27 @@
 #include <stdio.h>
 #include <readline/readline.h>
 
+void	print(void *ptr)
+{
+	char	*str;
+
+	str = ptr;
+	printf("%s ", str);
+}
+
 int	main(void)
 {
-	t_string	str;
+	char	*str;
+	t_list	*list;
 
-	str = readline(NULL);
+	while (1)
+	{
+		str = readline("minishell> ");
+		list = parsing(str);
+		wati_lstiter(list, print);
+		printf("\n");
+		wati_lstclear(&list, free);
+		free(str);
+	}
 	return (0);
 }
