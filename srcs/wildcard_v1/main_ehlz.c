5d89a738c8214db79b2c999853130e969205e055
Emile Halliez <ehalliez@2G5.42angouleme.fr>
2024-04-09 22:56:12 +0200

    version

--- srcs/wildcard_v1/main_ehlz.c
+++ srcs/wildcard_v1/main_ehlz.c
@@ -6,12 +6,26 @@
 /*   By: ehalliez <ehalliez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 15:52:03 by ehalliez          #+#    #+#             */
-/*   Updated: 2024/04/09 16:42:36 by ehalliez         ###   ########.fr       */
+/*   Updated: 2024/04/09 20:44:00 by ehalliez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
+
+int	count_stars(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		if (*str == '*')
+			count++;
+		str++;
+	}
+	return (count);
+}
 
 int	find_stars(char *str)
 {
@@ -63,69 +77,79 @@ static char	*get_second_check(char *search)
 	return (search);
 }
 
-int is_valid(char *content, char *search)
+int	star_at_start(char *search, char *content)
 {
+	int search_len;
+
+	while (*search == '*')
+		search++;
+	search_len = wati_strlen(search);
+	while (*content)
+	{
+		if (!wati_strncmp(content, search, search_len) && wati_strlen(content) >= wati_strlen(search))
+			return (1);
+		content++;
+	}
+	return (0);
+}
+
+int star_at_end(char *search, char *content)
+{
+	char	*first_check;
+	int		first_check_len;
+
+	first_check = get_first_check(search);
+	first_check_len = wati_strlen(first_check);
+	if (!wati_strncmp(content, first_check, first_check_len))
+	{
+		free(first_check);
+		return (1);	
+	}
+	free(first_check);
+	return (0);
+}
+
+int	clean_check(char *check, int number)
+{
+	if (check)
+		free(check);
+	return (number);
+}
+
+int	star_other_case(char *search, char *content)
+{
+	char	*first_check;
+	char	*second_check;
+	int		first_check_len;
+	int		second_check_len;
+
+	first_check = get_first_check(search);
+	first_check_len = wati_strlen(first_check);
+	second_check = get_second_check(search);
+	second_check_len = wati_strlen(second_check);
+	if (wati_strncmp(content, first_check, first_check_len))
+		return (clean_check(first_check, 0));	
+	while (*content)
+	{
+		if (!wati_strncmp(content, second_check, second_check_len) && wati_strlen(content) >= wati_strlen(second_check))
+				return (clean_check(first_check, 1));	
+		content++;
+	}
+	free(first_check);
+	return (0);	
+}
+
+int is_valid(char *content, char *search)
+{	
 	int	star_emplacement;
-	int	search_len;
 	
-	(void)content;
 	star_emplacement = find_stars(search);
 	if (star_emplacement == 1)
-	{
-		while (*search == '*')
-			search++;
-		search_len = wati_strlen(search);
-		while (*content)
-		{
-			if (!wati_strncmp(content, search, search_len) && wati_strlen(content) >= wati_strlen(search))
-				return (1);
-			content++;
-		}
-		return (0);
-	}
+		return (star_at_start(search, content));
 	else if (star_emplacement == 2)
-	{
-		char	*first_check;
-		char	*second_check;
-		int		first_check_len;
-		int		second_check_len;
-
-		first_check = get_first_check(search);
-		first_check_len = wati_strlen(first_check);
-		second_check = get_second_check(search);
-		second_check_len = wati_strlen(second_check);
-		if (wati_strncmp(content, first_check, first_check_len))
-		{
-			free(first_check);
-			return (0);	
-		}
-		while (*content)
-		{
-			if (!wati_strncmp(content, second_check, second_check_len) && wati_strlen(content) >= wati_strlen(second_check))
-			{
-				free(first_check);
-				return (1);
-			}
-			content++;
-		}
-		free(first_check);
-		return (0);	
-	}
+		return (star_other_case(search, content));
 	if (star_emplacement == 3)
-	{
-		char	*first_check;
-		int		first_check_len;
-
-		first_check = get_first_check(search);
-		first_check_len = wati_strlen(first_check);
-		if (!wati_strncmp(content, first_check, first_check_len))
-		{
-			free(first_check);
-			return (1);	
-		}
-		free(first_check);
-		return (0);
-	}
+		return (star_at_end(search, content));
 	return (0);
 }
 
