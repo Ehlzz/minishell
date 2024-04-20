bdbfb872e248874ac9c8d1adcabe8a6bcfb0a661
Benoit Darenne <bedarenn@3F3.42angouleme.fr>
2024-04-20 15:06:43 +0200

    fix |

--- srcs/btree/btree_build_tools.c
+++ srcs/btree/btree_build_tools.c
@@ -6,7 +6,7 @@
 /*   By: bedarenn <bedarenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 11:52:59 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/04/20 14:29:29 by bedarenn         ###   ########.fr       */
+/*   Updated: 2024/04/20 15:00:13 by bedarenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ t_bool	btree_build_cmd(t_btree **root, t_list **list, t_fds fds)
 	if (!cmd)
 		return (FALSE);
 	node = btree_create_node(cmd);
-	if (!node) // TEST
+	if (!node)
 	{
 		wati_fprintf(STDERR_FILENO, "%s: Error: alloc fail\n", NAME);
 		free_cmd(cmd);
@@ -50,7 +50,7 @@ t_bool	btree_build_oper(t_btree **root, t_list **list, t_fds fds)
 		return (FALSE);
 	}
 	node = btree_node_oper((*list)->content, fds);
-	if (!node) // TEST
+	if (!node)
 		return (FALSE);
 	free(get_token(*list)->str);
 	new_root(root, node);
@@ -60,6 +60,12 @@ t_bool	btree_build_oper(t_btree **root, t_list **list, t_fds fds)
 
 t_bool	btree_build_pipe(t_btree **root, t_list **list, t_fds fds)
 {
+	if (*root == NULL)
+	{
+		wati_fprintf(STDERR_FILENO,
+			"%s: Error: operator '%s'\n", NAME, get_token(*list)->str);
+		return (FALSE);
+	}
 	if ((get_cmd(*root)->oper == AND || get_cmd(*root)->oper == OR))
 		return (btree_build_oper(&(*root)->right, list, fds));
 	return (btree_build_oper(root, list, fds));
